82eca4e876ae868c4610572b0bf64ce6c6fcc3c0
HarshaVardhan <143703199+Harsha09n@users.noreply.github.com>
2025-10-25 11:34:37 +0530

    This is my First Commit

---
+++ EEmployeeManagementServicesApplication.java
@@ -0,0 +1,13 @@
+package com.EmployeesManagment.e.Employee_Management_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EEmployeeManagementServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EEmployeeManagementServicesApplication.class, args);
+	}
+
+}
---
+++ EEmployeeManagementServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EmployeesManagment.e.Employee_Management_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EEmployeeManagementServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
